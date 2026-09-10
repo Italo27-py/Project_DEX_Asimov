5adf73d626c835a4aaa6ef189411f3680f90e9b9
mazzer <matheusmazzerasimov.com.br>
2026-09-10 14:03:52 -0300

    remocao pendencias

--- app.py
+++ app.py
@@ -475,7 +475,6 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7= st.tabs(
         "🗓️ Histórico Multi-Ano",
         "🗂️ Projetos",
         "💡 Insights & Recomendações",
-        "⚠️ Pendências",
     ]
 )
 
